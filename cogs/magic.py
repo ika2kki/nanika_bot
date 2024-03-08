e35179da4146b4611f92672f3b81dfd10deeb7a2
Hadock <49624805+Hadock-is-ok@users.noreply.github.com>
2024-03-08 00:04:23 +0000

    8ball woa (#2)
    * is this good enough, lmk and ill do more :D tmr tho my gn time is up
    
    * Apply suggestions from code review
    
    Co-authored-by: ナニカ <101696371+ika2kki@users.noreply.github.com>
    
    * Update magic.py
    
    * dis gud?
    
    ---------
    
    Co-authored-by: NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
    Co-authored-by: ナニカ <101696371+ika2kki@users.noreply.github.com>

---
+++ cogs/magic.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+import random
+import core
+
+class Magic(core.nanika_cog):
+    @commands.command(name="8ball", aliases=["eightball"])
+    async def eightball(self, ctx, *, question):
+        answers = [
+            "it will happen",
+            "i decided yes",
+            "maaybe",
+            "uhhhhhhh- sureeee",
+            "yea",
+            "there is no doubts",
+            "it will def happen",
+            "u can rely on it if that makes u comfy",
+            "from what i see, nah",
+            "im unsure",
+            "try me again later",
+            "i cant tell you now, for your safety",
+            "its too blurry for me rn",
+            "you werent focusing?",
+            "not v reliable",
+            "nah",
+            "mmmm dunno",
+            "mah, maybe",
+            "there is doubts",
+            "it wont happen"
+        ]
+        await ctx.send(f"magic eight ball say this: {random.choice(answers)}, in response to your question:\n{question}"[:2000])
+
+
+async def setup(bot):
+    await bot.add_cog(Magic(bot))
